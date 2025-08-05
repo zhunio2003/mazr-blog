a37869f5a7aa3f54d8312a4d5e6de9ca08d458d1
zhunio2003 <190935222+zhunio2003@users.noreply.github.com>
2025-08-05 13:57:59 -0500

    Create Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
+import Header from './components/common/Header'
 import './styles/globals.css'
 
 function App() {
   return (
     <div className='main-layout'>
-      <header className='container'>
-        <h1>mazr</h1>
-      </header>
-
+      <Header/>
       <main className='content container'>
         <p>Stream processing 1</p>
         <p>Stream processing 1</p>
--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,10 +1,40 @@
 import { Search } from "lucide-react";
 import styles from "../../styles/components/Header.module.css"
+import logo from "../../assets/logomazr.svg"
 
 const Header = () => {
     return (
-        <header className="{style.mainHeader}">
-            
+        <header className={styles.mainHeader}>
+            <div className={styles.headerContainer}>
+                
+                {/* Logo centrado */}
+                <div className={styles.logo}>
+                    <img src={logo} alt="mazr"/>
+                </div>
+
+                
+                {/* Navigation menu */}
+                <nav className={styles.mainNav}>
+                    <a href="#" className={`${styles.navLink} ${styles.active}`}>Home</a>
+                    <a href="#" className={styles.navLink}>Programming</a>
+                    <a href="#" className={styles.navLink}>Neuroscience</a>
+                    <a href="#" className={styles.navLink}>Physical</a>
+                </nav>
+
+                {/* Search */}
+                <div className={styles.searchSection}>
+                    <div className={styles.searchBox}>
+                        <input 
+                            type="text"
+                            placeholder="SEARCH"
+                            className={styles.searchInput}
+                        />
+                         <Search size={16} className={styles.searchIcon}/>
+                    </div>
+                </div>
+            </div> 
         </header>
     )
 }
+
+ export default Header;
