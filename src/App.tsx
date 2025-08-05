744a312f45c5a90d7a6ceaa049e0ca7b14370e08
zhunio2003 <190935222+zhunio2003@users.noreply.github.com>
2025-08-05 10:10:54 -0500

    Commit initial

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './styles/globals.css'
+
+function App() {
+  return (
+    <div className='main-layout'>
+      <header className='container'>
+        <h1>mazr</h1>
+      </header>
+
+      <main className='content container'>
+        <p>Stream processing 1</p>
+        <p>Stream processing 1</p>
+        <p>Stream processing 1</p>
+
+      </main>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/common/Header.tsx
@@ -0,0 +1,10 @@
+import { Search } from "lucide-react";
+import styles from "../../styles/components/Header.module.css"
+
+const Header = () => {
+    return (
+        <header className="{style.mainHeader}">
+            
+        </header>
+    )
+}
